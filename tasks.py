bea3bdb80f6602032fd9e8699c24f4a0b37a62b1
Pablo Padilla <pgpb.padilla@gmail.com>
2018-02-10 12:35:24 -0800

    new: doc: Add description for tasks.

--- tasks.py
+++ tasks.py
@@ -3,20 +3,19 @@ from invoke import task
 
 @task
 def down(ctx):
+    """Shutdown all containers"""
     ctx.run('docker-compose down', echo=True)
 
 
 @task
 def up(ctx):
+    """Start RabbitMQ, MongoDB and Tornado consumer."""
     ctx.run('docker-compose build')
     ctx.run('docker-compose up -d', echo=True)
 
 
-@task
+@task(pre=[up])
 def publish(ctx):
+    """Boot up all containers and start publishing messages to RabbitMQ."""
     ctx.run('python publisher/rmq_publisher.py')
 
-
-@task
-def consume(ctx):
-    ctx.run('python consumer.py')
