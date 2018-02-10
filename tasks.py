eab51b18721ef2894c43dc25d552e830f555d2a4
Pablo Padilla <pgpb.padilla@gmail.com>
2018-02-10 11:21:50 -0800

    new: dev: Add Async tornado consumer.

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 from invoke import task
-
+import consumer
 
 @task
 def up(ctx):
@@ -12,29 +12,5 @@ def publish(ctx):
 
 
 @task
-def consume(_):
-    import pika
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost')
-    )
-    channel = connection.channel()
-    exchange = 'test-exchange'
-    channel.exchange_declare(
-        exchange=exchange,
-        exchange_type='fanout',
-        auto_delete=True
-    )
-    result = channel.queue_declare(exclusive=True)
-    queue_name = result.method.queue
-    channel.queue_bind(exchange=exchange, queue=queue_name)
-    print(' [*] Waiting for logs. To exit press CTRL+C')
-
-    def callback(ch, method, properties, body):
-        print(" [x] %r" % body)
-
-    channel.basic_consume(
-        callback,
-        queue=queue_name,
-        no_ack=True
-    )
-    channel.start_consuming()
+def consume(ctx):
+    ctx.run('python consumer.py')
